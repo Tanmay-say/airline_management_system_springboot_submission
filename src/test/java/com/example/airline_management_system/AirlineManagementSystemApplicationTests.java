13f6d276365564b5fa314695b83a3ec0495b5276
Tanmay-say Ubuntu Macbook <tanmaysayare12345@gmail.com>
2025-03-21 23:44:29 +0530

    1 Test Case passed

--- src/test/java/com/example/airline_management_system/AirlineManagementSystemApplicationTests.java
+++ src/test/java/com/example/airline_management_system/AirlineManagementSystemApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.airline_management_system;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class AirlineManagementSystemApplicationTests {
+@AutoConfigureMockMvc
+class FlightControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void testGetAllFlights() throws Exception {
+		mockMvc.perform(get("/flights"))
+				.andExpect(status().isOk());  // Always expects 200 OK
 	}
-
 }
